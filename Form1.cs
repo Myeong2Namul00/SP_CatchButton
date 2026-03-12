1797e231903660e431e08ad79ad75c56417b2ef8
김명이 <myeongyevillain@gmail.com>
2026-03-12 15:12:09 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace CS_Week02_22017011_CatchButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
